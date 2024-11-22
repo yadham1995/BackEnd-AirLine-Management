fc2d5fa657b7e1ad33da9d856a12d63a08ce2f7c
LAPTOP-STB08R8Q\YOUSSEF <yossefadham95@gmail.com>
2024-11-22 11:33:31 +0200

    dml

---
+++ dml.sql
@@ -0,0 +1,17 @@
+INSERT INTO user (user_name, employee_id, iata_code) VALUES
+('youssef', 'E12345', 'JFK'),
+('ayman', 'E12346', 'LHR');
+
+INSERT INTO sales_person (sales_person_code, name) VALUES
+('SP001', 'youssef mostafa'),
+('SP002', 'ayman gawad');
+
+INSERT INTO customer (customer_code, name) VALUES
+('CUST123', 'youssef mostafa'),
+('CUST124', 'ayman jawad');
+
+INSERT INTO carrier_code (code, name) VALUES 
+('AA', 'American Airlines'),
+('DL', 'Delta Airlines'),
+('UA', 'United Airlines');
+
